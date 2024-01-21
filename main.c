c3ec31c17b849a9551e6612684c821590fd3c216
NathanPradelle <nathan.pradelle77@gmail.com>
2024-01-21 19:56:05 +0100

    add lib sdl + test

---
+++ main.c
@@ -0,0 +1,43 @@
+#include </mingw64/include/SDL2/SDL.h>
+
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        return 1;
+    }
+
+    SDL_Window *fenetre = SDL_CreateWindow("Ma fenêtre", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (fenetre == NULL) {
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        SDL_DestroyWindow(fenetre);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Boucle d'événements SDL
+    SDL_Event event;
+    int running = 1;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    // Libération des ressources
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
+
+    return 0;
+}
